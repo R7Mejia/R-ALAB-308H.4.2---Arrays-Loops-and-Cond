3d797ac8305fccb4d10a2224a0c338359f878d03
Roberto Mejia <robertomejiar7@gmail.com>
2023-10-21 17:52:25 -0400

    FIZZBUZZ & WILD-LIFE completed

--- main.js
+++ main.js
@@ -1,22 +1,66 @@
-////////////////EXERCISE ONE////////////
-///////////////Easy Going//////////////
+//////////////EXERCISE ONE////////////
+/////////////Easy Going//////////////
 //Write a for loop that will log the numbers 1 through 20.
-/*for (let i = 0; i <= 20; i++) {
-    console.log(i);
-}*/
+/*for (let a = 0; a <= 20; a++) {
+    //console.log(a);
+}
 
 ////////////////EXERCISE TWO////////////
 ///////////////Get Even////////////////
 //Write a for loop that will log only the even numbers in 0 through 200.
-for (let i = 0; i <= 200; i++){
-    if (i % 2  === 0) {
-        console.log(i)
+for (let b = 0; b <= 200; b++){
+    if (b % 2  === 0) {
+        //console.log(b)
     }
-}
-/////////////////EXERCISE TWO////////////
+}*/
+/////////////////EXERCISE THREE////////////
 ////////////////Fizz Buzz///////////////
 // Write a Javascript Application that logs all numbers from 1 - 100.
-
+/*for (let c = 1; c <= 100; c++){
+    console.log(c)
+}
 // If a number is divisible by 3, log "Fizz" instead of the number.
 // If a number is divisible by 5, log "Buzz" instead of the number.
 // If a number is divisible by 3 and 5, log "FizzBuzz" instead of the number.
+////////////////////////////////////////////////////
+////////////////THE "FIZZBUZZ" PART////////////////
+//////////////////////////////////////////////////
+const c1 = 105;//this value can change IF I want to explore more options
+
+if (c1 % 3 === 0 && c1 % 5 === 0) {
+    console.log("FizzBuzz");
+} else if (c1 % 3 === 0) {
+    console.log("Fizz");
+} else if (c1 % 5 === 0) {
+    console.log("Buzz");
+} else {
+    console.log(c1);
+}*/
+////////////////EXERCISE FOUR////////////
+///////////////Wild Wild Life///////////
+//Use the following arrays to answer the questions below(name, species, age, hometown).
+/*const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"];
+const sharky = ["Sharky", "shark", 20, "Left Coast"];
+const plantee = ["Plantee", "plant", 5000, "Mordor"];
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"];
+const dart = ["D'Art", "Demogorgan Dog", 2, "Upside Down"];
+// Plantee just had her birthday; change Plantee's array to reflect her being a year older.
+const planteeNewAge = plantee.splice(2, 1, 5001)//splice() and slice() c'mon!
+console.log(plantee)
+// Change Wolfy's hometown from "Yukon Territory" to "Gotham City".
+const wolfyNewTown = wolfy.splice(3, 1, "Gotham City");//(3 = index, 1 = how many elements should be replace, "" = the replacement)
+console.log(wolfy)
+// Give D'Art a second hometown by adding "Hawkins"
+let dartSecondTown = dart.push("Hawkins");
+console.log(dart)
+// Porgee decides that Wolfy can't be named "Wolfy" anymore. Remove "Wolfy" from the wolfy array and replace it with "Gameboy".
+let newGuy = wolfy.splice("Gameboy");
+console.log(wolfy)*/
+////////////////EXERCISE FIVE////////////
+
+
+
+
+
+
+
