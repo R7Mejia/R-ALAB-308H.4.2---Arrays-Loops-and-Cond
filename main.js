dac7a9ad013f2adfa7e8cd917315d8c4e8d7620b
Roberto Mejia <robertomejiar7@gmail.com>
2023-10-21 13:18:33 -0400

    first commit of the first two exercises

---
+++ main.js
@@ -0,0 +1,22 @@
+////////////////EXERCISE ONE////////////
+///////////////Easy Going//////////////
+//Write a for loop that will log the numbers 1 through 20.
+/*for (let i = 0; i <= 20; i++) {
+    console.log(i);
+}*/
+
+////////////////EXERCISE TWO////////////
+///////////////Get Even////////////////
+//Write a for loop that will log only the even numbers in 0 through 200.
+for (let i = 0; i <= 200; i++){
+    if (i % 2  === 0) {
+        console.log(i)
+    }
+}
+/////////////////EXERCISE TWO////////////
+////////////////Fizz Buzz///////////////
+// Write a Javascript Application that logs all numbers from 1 - 100.
+
+// If a number is divisible by 3, log "Fizz" instead of the number.
+// If a number is divisible by 5, log "Buzz" instead of the number.
+// If a number is divisible by 3 and 5, log "FizzBuzz" instead of the number.
