b5172534d973fcdfa456ab00fddc340f145f1c9b
Roberto Mejia <robertomejiar7@gmail.com>
2023-10-21 23:18:48 -0400

    almost done

--- main.js
+++ main.js
@@ -56,7 +56,58 @@ console.log(dart)
 // Porgee decides that Wolfy can't be named "Wolfy" anymore. Remove "Wolfy" from the wolfy array and replace it with "Gameboy".
 let newGuy = wolfy.splice("Gameboy");
 console.log(wolfy)*/
-////////////////EXERCISE FIVE////////////
+////////////////EXERCISE FIVE/////////////////////
+///////////////Yell at the Ninja Turtles/////////
+//Create an array with the members of the ninja turtles (Donatello, Leonardo, Raphael, Michaelangelo).
+/*const ninjaTurtles = ['Donatello', 'Leonardo', 'Raphael', 'Michaelangelo'];
+//Use a for of loop to call toUpperCase() on each of them and print out the result.
+const newTurtles = [];
+for (let d = 0; d < ninjaTurtles.length; d++){
+    newTurtles.push(ninjaTurtles[d].toUpperCase());
+    console.log(newTurtles);
+}*/
+////////////////EXERCISE SIX+/////////////////////
+///////////////Methods, Revisited////////////////
+//Here is a list of favorite movies:
+
+const favMovies = ['Jaws', 'The Fellowship of the Ring',
+    'Howl\'s Moving Castle', 'Django Unchained', 'Cloud Atlas',
+    'The Usual Suspects', 'Toy Story', 'Conan the Barbarian',
+    'Titanic', 'Harry Potter', 'Fried Green Tomatoes',
+    'Volver', 'Oculus', 'Seven', 'Black Panther',
+    'Harry Potter', 'Imitation of Life', 'Snatch', 'Fast and Furious'];
+    
+/*//console log the index of Titanic
+console.log(favMovies[8]) 
+//Use the.sort method.
+favMovies.sort();
+console.log(favMovies);
+// Use the method pop.
+favMovies.pop();
+console.log(favMovies)
+// push "Guardians of the Galaxy".
+favMovies.push("Guardians of the Galaxy");
+console.log(favMovies);
+// Reverse the array.
+favMovies.reverse();
+console.log(favMovies)
+// Use the shift method.
+favMovies.shift();
+console.log(favMovies);
+// splice "Django Unchained" and add "Avatar" (try finding the index of "Django Unchained" instead of counting it yourself).
+const djangoIndex = favMovies.indexOf("Django Unchained");
+favMovies.splice(djangoIndex, 1, "Avatar");
+console.log(favMovies);*/
+// slice the last half of the array. (Challenge yourself and try to programatically determine the middle of the array rather than counting it and hard coding it.)
+// const middleIndex = Math.floor(favMovies.length / 2);
+// favMovies.slice(middleIndex, middleIndex);//I DON'T REALLY GET WHAT THEY WNT ME TO DO HERE. I HAD TO GOOGLE THIS...
+// // Store the value of your slice in a variable and console.log it. (ASK FOR HELP)
+
+// console.log your final results.
+// After running the above tasks, console.log the index of "Fast and Furious."
+/*const fastAndFuriousIndex = favMovies.indexOf("Fast and Furious");
+console.log(fastAndFuriousIndex);*/
+
 
 
 
