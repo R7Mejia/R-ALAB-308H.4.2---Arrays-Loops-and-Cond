69915ccf1968cb0e75a4e01cdd74ddac6acc2170
Roberto Mejia <robertomejiar7@gmail.com>
2023-10-23 09:03:48 -0400

    Exercise 8 is completed

--- main.js
+++ main.js
@@ -69,7 +69,7 @@ for (let d = 0; d < ninjaTurtles.length; d++){
 ////////////////EXERCISE SIX+/////////////////////
 ///////////////Methods, Revisited////////////////
 //Here is a list of favorite movies:
-/*
+
 const favMovies = ['Jaws', 'The Fellowship of the Ring',
     'Howl\'s Moving Castle', 'Django Unchained', 'Cloud Atlas',
     'The Usual Suspects', 'Toy Story', 'Conan the Barbarian',
@@ -77,7 +77,7 @@ const favMovies = ['Jaws', 'The Fellowship of the Ring',
     'Volver', 'Oculus', 'Seven', 'Black Panther',
     'Harry Potter', 'Imitation of Life', 'Snatch', 'Fast and Furious'];
     
-//console log the index of Titanic
+/*/console log the index of Titanic
 console.log(favMovies[8]) 
 //Use the.sort method.
 favMovies.sort();
@@ -98,34 +98,79 @@ console.log(favMovies);
 const djangoIndex = favMovies.indexOf("Django Unchained");
 favMovies.splice(djangoIndex, 1, "Avatar");
 console.log(favMovies);*/
-// slice the last half of the array. (Challenge yourself and try to programatically determine the middle of the array rather than counting it and hard coding it.)
-// const middleIndex = Math.floor(favMovies.length / 2);
-// favMovies.slice(middleIndex, middleIndex);//I DON'T REALLY GET WHAT THEY WNT ME TO DO HERE. I HAD TO GOOGLE THIS...
-// // Store the value of your slice in a variable and console.log it. (ASK FOR HELP)
 
+/////////////////HELP NEEDED!!!!!////////////
+////////////////////////////////////////////
+// slice the last half of the array. (Challenge yourself and try to programatically determine the middle of the array rather than counting it and hard coding it.)
+// Store the value of your slice in a variable and console.log it.
 // console.log your final results.
+/*
+const middleIndex = Math.floor(favMovies.length / 2);
+favMovies.slice(middleIndex);//I DON'T REALLY GET WHAT THEY WNT ME TO DO HERE. 
+
+console.log(middleIndex)//this consoles log 9, and that's the middle of the array. But what are the other instructins about?
+*/
+//////////////////////////////////////////
+/////////////////////////////////////////
+
 // After running the above tasks, console.log the index of "Fast and Furious."
-/*const fastAndFuriousIndex = favMovies.indexOf("Fast and Furious");
-console.log(fastAndFuriousIndex);*/
+// const fastAndFuriousIndex = favMovies.indexOf("Fast and Furious");
+// console.log(fastAndFuriousIndex);//18
 
 ////////////////EXERCISE SEVEN/////////////////////
 ///////////////Where is Waldo////////////////////
 
 const whereIsWaldo = [["Timmy", "Frank"], "Eggbert",
-                      ["Lucinda", "Jacc", "Neff", "Snoop"],
-                      ["Petunia", ["Baked Goods", "Waldo"]]];
+                    ["Lucinda", "Jacc", "Neff", "Snoop"],
+                    ["Petunia", ["Baked Goods", "Waldo"]]];
+                    
 // Remove Eggbert (hint look at the slice/splice methods).
-/*whereIsWaldo.splice(1, 1);
-console.log(whereIsWaldo)
+// whereIsWaldo.splice(1, 1);
+// console.log(whereIsWaldo)
+
 // Change "Neff" to "No One".*/
-//const neffIndex = whereIsWaldo.indexOf("Neff");
-whereIsWaldo.splice([2][2], 1, 'No One');
-console.log(whereIsWaldo);//Still not working
+
+// const neffIndex = whereIsWaldo[2].indexOf("Neff");
+// whereIsWaldo[2].splice(neffIndex, 1, "No One");
+// console.log(whereIsWaldo);
 
 // Access and console.log "Waldo".
+const waldoIndex = whereIsWaldo.indexOf("Waldo");
+//console.log(waldoIndex);//IT DOESN'T WORK 
+
+////////////////EXERCISE EIGHT/////////////////////
+///////////////Excited Kitten/////////////////////
+// Write code that logs "Love me, pet me! HSSSSSS!" 20 times.
+// For every even number in your loop, log "...human... why you taking pictures of me...?", "...the catnip made me do it...", or "...why does the red dot always get away...?" at random.
+const kittyTalkie = ["love me", "pet me!", "HSSSSSS"];
+
+let kitty = 0;
 
 
+////////////////EXERCISE NINE/////////////////////
+///////////////Find the Median///////////////////
+//Find the median number in the following nums array, then console.log that number.
+/*
+//Hint: If you check the length of the array divided by two, you might get not get a whole number. In which case, look into Math.floor().
+const nums = [14,11,16,15,13,16,15,17,19,11,12,14,19,11,15,17,11,18,12,17,12,71,18,15,12];
+
+// Sort the array in ascending order.
+nums.sort((a, b) => a - b);
+
+// Calculate the middle index of the array. (math.floor is used)
+const middlIndex = Math.floor(nums.length / 2);
+
+// Find the median.
+let median;
+if (nums.length % 2 === 0) {
+    median = (nums[middlIndex] + nums[middlIndex - 1]) / 2;
+} else {
+    median = nums[middlIndex];
+}
+
+// Console.log the median.
+console.log(median);
+*/
 
 
-
-
+//////////////////THE END//////////////////
