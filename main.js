637375323e00a34490edf238c45d18e41286b58a
Roberto Mejia <robertomejiar7@gmail.com>
2023-10-23 21:32:24 -0400

    Final commit

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 //////////////EXERCISE ONE////////////
 /////////////Easy Going//////////////
+/*
 //Write a for loop that will log the numbers 1 through 20.
-/*for (let a = 0; a <= 20; a++) {
+for (let a = 0; a <= 20; a++) {
     //console.log(a);
 }
 
@@ -12,7 +13,7 @@ for (let b = 0; b <= 200; b++){
     if (b % 2  === 0) {
         //console.log(b)
     }
-}*/
+}
 /////////////////EXERCISE THREE////////////
 ////////////////Fizz Buzz///////////////
 // Write a Javascript Application that logs all numbers from 1 - 100.
@@ -35,7 +36,7 @@ if (c1 % 3 === 0 && c1 % 5 === 0) {
     console.log("Buzz");
 } else {
     console.log(c1);
-}*/
+}
 ////////////////EXERCISE FOUR////////////
 ///////////////Wild Wild Life///////////
 //Use the following arrays to answer the questions below(name, species, age, hometown).
@@ -55,30 +56,45 @@ let dartSecondTown = dart.push("Hawkins");
 console.log(dart)
 // Porgee decides that Wolfy can't be named "Wolfy" anymore. Remove "Wolfy" from the wolfy array and replace it with "Gameboy".
 let newGuy = wolfy.splice("Gameboy");
-console.log(wolfy)*/
+console.log(wolfy)
 ////////////////EXERCISE FIVE/////////////////////
 ///////////////Yell at the Ninja Turtles/////////
 //Create an array with the members of the ninja turtles (Donatello, Leonardo, Raphael, Michaelangelo).
-/*const ninjaTurtles = ['Donatello', 'Leonardo', 'Raphael', 'Michaelangelo'];
+const ninjaTurtles = ['Donatello', 'Leonardo', 'Raphael', 'Michaelangelo'];
 //Use a for of loop to call toUpperCase() on each of them and print out the result.
 const newTurtles = [];
 for (let d = 0; d < ninjaTurtles.length; d++){
     newTurtles.push(ninjaTurtles[d].toUpperCase());
     console.log(newTurtles);
-}*/
+}
 ////////////////EXERCISE SIX+/////////////////////
 ///////////////Methods, Revisited////////////////
 //Here is a list of favorite movies:
 
-const favMovies = ['Jaws', 'The Fellowship of the Ring',
-    'Howl\'s Moving Castle', 'Django Unchained', 'Cloud Atlas',
-    'The Usual Suspects', 'Toy Story', 'Conan the Barbarian',
-    'Titanic', 'Harry Potter', 'Fried Green Tomatoes',
-    'Volver', 'Oculus', 'Seven', 'Black Panther',
-    'Harry Potter', 'Imitation of Life', 'Snatch', 'Fast and Furious'];
-    
-/*/console log the index of Titanic
-console.log(favMovies[8]) 
+const favMovies = [
+  "Jaws",
+  "The Fellowship of the Ring",
+  "Howl's Moving Castle",
+  "Django Unchained",
+  "Cloud Atlas",
+  "The Usual Suspects",
+  "Toy Story",
+  "Conan the Barbarian",
+  "Titanic",
+  "Harry Potter",
+  "Fried Green Tomatoes",
+  "Volver",
+  "Oculus",
+  "Seven",
+  "Black Panther",
+  "Harry Potter",
+  "Imitation of Life",
+  "Snatch",
+  "Fast and Furious",
+];
+
+//console log the index of Titanic
+console.log(favMovies[8])
 //Use the.sort method.
 favMovies.sort();
 console.log(favMovies);
@@ -97,19 +113,19 @@ console.log(favMovies);
 // splice "Django Unchained" and add "Avatar" (try finding the index of "Django Unchained" instead of counting it yourself).
 const djangoIndex = favMovies.indexOf("Django Unchained");
 favMovies.splice(djangoIndex, 1, "Avatar");
-console.log(favMovies);*/
+console.log(favMovies);
 
 /////////////////HELP NEEDED!!!!!////////////
 ////////////////////////////////////////////
 // slice the last half of the array. (Challenge yourself and try to programatically determine the middle of the array rather than counting it and hard coding it.)
 // Store the value of your slice in a variable and console.log it.
 // console.log your final results.
-/*
+
 const middleIndex = Math.floor(favMovies.length / 2);
-favMovies.slice(middleIndex);//I DON'T REALLY GET WHAT THEY WNT ME TO DO HERE. 
+favMovies.slice(middleIndex);//I DON'T REALLY GET WHAT THEY WNT ME TO DO HERE.
 
 console.log(middleIndex)//this consoles log 9, and that's the middle of the array. But what are the other instructins about?
-*/
+
 //////////////////////////////////////////
 /////////////////////////////////////////
 
@@ -120,23 +136,26 @@ console.log(middleIndex)//this consoles log 9, and that's the middle of the arra
 ////////////////EXERCISE SEVEN/////////////////////
 ///////////////Where is Waldo////////////////////
 
-const whereIsWaldo = [["Timmy", "Frank"], "Eggbert",
-                    ["Lucinda", "Jacc", "Neff", "Snoop"],
-                    ["Petunia", ["Baked Goods", "Waldo"]]];
-                    
-// Remove Eggbert (hint look at the slice/splice methods).
-// whereIsWaldo.splice(1, 1);
-// console.log(whereIsWaldo)
+const whereIsWaldo = [
+    ["Timmy", "Frank"],
+    "Eggbert",
+    ["Lucinda", "Jacc", "Neff", "Snoop"],
+    ["Petunia", ["Baked Goods", "Waldo"]],
+];
 
-// Change "Neff" to "No One".*/
+//Remove Eggbert (hint look at the slice/splice methods).
+whereIsWaldo.splice(1, 1);
+console.log(whereIsWaldo)
+
+//Change "Neff" to "No One".
 
 // const neffIndex = whereIsWaldo[2].indexOf("Neff");
 // whereIsWaldo[2].splice(neffIndex, 1, "No One");
 // console.log(whereIsWaldo);
 
 // Access and console.log "Waldo".
-const waldoIndex = whereIsWaldo.indexOf("Waldo");
-//console.log(waldoIndex);//IT DOESN'T WORK 
+const waldoIndex = whereIsWaldo[2][1][1];
+console.log(waldoIndex);
 
 ////////////////EXERCISE EIGHT/////////////////////
 ///////////////Excited Kitten/////////////////////
@@ -144,13 +163,20 @@ const waldoIndex = whereIsWaldo.indexOf("Waldo");
 // For every even number in your loop, log "...human... why you taking pictures of me...?", "...the catnip made me do it...", or "...why does the red dot always get away...?" at random.
 const kittyTalkie = ["love me", "pet me!", "HSSSSSS"];
 
-let kitty = 0;
-
-
+for (let e = 0; e <= 20; e++) {
+  if (e % 2 === 0) {
+    const randomIndex = Math.floor(Math.random() * kittyTalkie.length);
+    console.log(kittyTalkie[randomIndex]);
+  } else {
+    console.log(
+      "Yo, human... why you taking pictures of me...? HSSSSSSS 😾 😾😾 😾"
+    );
+  }
+}
 ////////////////EXERCISE NINE/////////////////////
 ///////////////Find the Median///////////////////
 //Find the median number in the following nums array, then console.log that number.
-/*
+
 //Hint: If you check the length of the array divided by two, you might get not get a whole number. In which case, look into Math.floor().
 const nums = [14,11,16,15,13,16,15,17,19,11,12,14,19,11,15,17,11,18,12,17,12,71,18,15,12];
 
@@ -171,6 +197,5 @@ if (nums.length % 2 === 0) {
 // Console.log the median.
 console.log(median);
 */
-
 
 //////////////////THE END//////////////////
---
+++ readme.dm
@@ -0,0 +1,16 @@
+Arrays, Loops, and Conditionals
+
+This assignment is designed to help you learn about arrays, loops, and conditionals in JavaScript.
+
+Arrays are data structures that can store a collection of items. Each item in an array is assigned a unique index, which is used to access the item.
+
+Loops are used to execute a block of code multiple times. There are three types of loops in JavaScript:
+
+for loops are used to execute a block of code a fixed number of times.
+while loops are used to execute a block of code while a condition is true.
+do...while loops are similar to while loops, but they will execute the block of code at least once, even if the condition is false.
+Conditionals are used to control the flow of execution of a program. There are three types of conditionals in JavaScript:
+
+if statements are used to execute a block of code if a condition is true.
+else statements are used to execute a block of code if a condition is false.
+else if statements are used to execute a block of code if a condition is true, but other conditions have already been evaluated as false.
